44eed912dcac69a7b8b44f50e76f4588970c42a9
Gabriel <gszcode@gmail.com>
2023-07-25 23:14:32 -0300

    fuente configurada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import Index from '@/components/Index'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: ['200', '300', '400', '500', '600', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap'
+})
+
 export default function Home() {
   return (
-    <main>
-      <h1>Holis</h1>
+    <main className={poppins.className}>
+      <Index />
     </main>
   )
 }
