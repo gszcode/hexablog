ed084c3a6d05fb9ca3d0e9d36867e7ff265eecf7
Gabriel <gszcode@gmail.com>
2023-08-07 20:04:08 -0300

    export de icono

--- src/components/icons/MenuIcon.tsx
+++ src/components/icons/MenuIcon.tsx
@@ -1,5 +1,5 @@
 import { SVGProps } from 'react'
-const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+export const Menu = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     height="1em"
@@ -9,4 +9,3 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
     <path d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z" />
   </svg>
 )
-export default SvgComponent
