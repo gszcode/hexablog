0597d05ef2bd0b4b44fc33f81db6b8a4c2509d64
Gabriel <gszcode@gmail.com>
2023-08-07 21:57:10 -0300

    datos de preguntas agregados

--- src/utils/dataMapping.ts
+++ src/utils/dataMapping.ts
@@ -6,6 +6,12 @@ interface Data {
   description?: string
 }
 
+interface DataQuestions {
+  questionNum: string
+  question: string
+  answer: string
+}
+
 export const adviceData: Array<Data> = [
   {
     id: 1,
@@ -92,5 +98,65 @@ export const ebooksData: Array<Data> = [
     title: 'entrenamiento principiantes',
     description:
       'Información simple y de calidad, para que puedas sacarle el mejor partido a tus entrenamientos y saber a ciencia cierta si estás haciendo las cosas bien para llegar a tu meta.'
+  }
+]
+
+export const questionsData: Array<DataQuestions> = [
+  {
+    questionNum: '1.',
+    question: '¿CUANTO TIEMPO TARDA EN LLEGARME LA PLANIFICACIÓN?',
+    answer:
+      'Una vez que se reciba el pago y la planilla completada, se le mandará la rutina entre el sabado y domingo, para comenzar con la rutina el lunes con las dudas ya resueltas.'
+  },
+  {
+    questionNum: '2.',
+    question: '¿CADA CUANTO SE CAMBIA LA RUTINA Y/O DIETA?',
+    answer:
+      'Depende exclusivamente de vos, se te haran chequeos mensuales/semanales/diarios (depende del pack que elijas) para ver como estas respondiendo a la rutina, si no estas respondiendo bien se te RETOCARA la rutina o dieta, en caso de no necesitar retocarla, no hará falta gastar cartas bajo la manga y seguirás con la misma rutina hasta que el estimulo te quede corto.'
+  },
+  {
+    questionNum: '3.',
+    question: '¿CÓMO ES EL SEGUIMIENTO?',
+    answer:
+      'Dependiendo del pack que elijas, el PACK premium es un seguimiento diario por wpp, el pack platino es un seguimiento semanal por vía E-MAIL, y el pack básico es un seguimiento diario mensual por vía E-MAIL.'
+  },
+  {
+    questionNum: '4.',
+    question: '¿CUANDO EMPIEZA A CORRER EL PAGO?',
+    answer: `Nosotros tomamos a los clientes desde el martes hasta el domingo de la anterior semana a comenzar con la rutina, y el pago empieza a correr el día LUNES que es cuando comienza con el programa. 
+      
+    Es decir, si abonas el día miercoles 26/08, el pago comenzara a correr el lunes 31/08 que es cuando comenzaras con el programa.`
+  },
+  {
+    questionNum: '5.',
+    question: '¿EL PRECIO ESTA EN PESOS ARGENTINOS?',
+    answer:
+      'Si! a los extranjero se le mandara un LINK de paypal o por western union.'
+  },
+  {
+    questionNum: '6.',
+    question: '¿SE PUEDE REMBOLSAR?',
+    answer:
+      'Se realizaran devoluciones ÚNICAMENTE en las primeras 72hs posteriores al pago, una vez pasado el tiempo, no habrá devoluciones.'
+  },
+  {
+    questionNum: '7.',
+    question: '¿COMO ME CONTACTO PARA COMENZAR?',
+    answer:
+      'Puedes enviarnos un mensáje desde el formulario, ingresando a nuestras redes sociales o enviarnos un email.'
+  },
+  {
+    questionNum: '8.',
+    question: '¿AL MOMENTO DE RENOVAR EL PLAN, CUANDO TENGO QUE PAGAR?',
+    answer: `Una vez acabado el primer mesociclo y el cliente decide renovar la asesoría, deberá abonar el mismo en la semana que recibe el plan dando un plazo máximo de otra semana más, es decir, 2 semanas de tiempo tiene hasta pagar el mesociclo enviado. 
+      
+    Si no se recibe el pago a partir de la segunda semana, se le cobrará un 30% de recargo.`
+  },
+  {
+    questionNum: '9.',
+    question: '¿CUANTO DURA EL ASESORAMIENTO?',
+    answer: `Los planes son de 4 semanas (un mesociclo), salvo que abones el PLAN TRIMESTRAL donde el plan dura 12 semanas (3 mesociclos, 4 semanas cada uno) 
+      
+      Cada 4 semanas se debería renovar el plan.`
   }
 ]
